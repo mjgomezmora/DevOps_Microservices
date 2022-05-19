260cdc187fca80b5022043c210147f4495d4838b
EC2 Default User <ec2-user@ip-172-31-54-212.ec2.internal>
2022-05-19 17:46:23 +0000

    Run kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,11 +5,11 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=devopsmicroservices
+dockerpath=491808826414.dkr.ecr.us-east-1.amazonaws.com/devopsmicroservices:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deploy devopsmicroservices --image=491808826414.dkr.ecr.us-east-1.amazonaws.com/devopsmicroservices:latest
+kubectl run devopsmicroservices-app --image=$dockerpath --port=80 --labels app=devopsmicroservices-app
 
 # Step 3:
 # List kubernetes pods
@@ -17,5 +17,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl run devopsmicroservicesapp --image=491808826414.dkr.ecr.us-east-1.amazonaws.com/devopsmicroservices:latest
-kubectl port-forward pod/devopsmicroservicesapp --address 0.0.0.0 5000:5000
+kubectl port-forward devopsmicroservices-app 80:80
