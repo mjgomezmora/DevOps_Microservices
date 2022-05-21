f7bf089bea93d51bb79584786f4ea27d1122b966
EC2 Default User <ec2-user@ip-172-31-54-212.ec2.internal>
2022-05-21 06:38:22 +0000

    Run kubernetes

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -10,6 +10,7 @@ dockerpath=devopsmicroservices
 
 # Step 2:  
 # Authenticate & tag
+
 echo "Docker ID and Image: $dockerpath"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 491808826414.dkr.ecr.us-east-1.amazonaws.com
 docker tag devopsmicroservices:latest 491808826414.dkr.ecr.us-east-1.amazonaws.com/devopsmicroservices:latest
