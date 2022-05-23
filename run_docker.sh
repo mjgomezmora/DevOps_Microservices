3f8f37c865feac8619b3b90458132c1d57b7c31c
EC2 Default User <ec2-user@ip-172-31-54-212.ec2.internal>
2022-05-23 10:15:29 +0000

    Moving files to correct place

---
+++ run_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=mjgmora/devopsmicroservices-repository .
+
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+sudo /home/ec2-user/.devops/bin/python3 app.py
+docker run -it mjgmora/devopsmicroservices-repository bash
---
+++ run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=mjgmora/devopsmicroservices:latest
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+#kubectl create deploy devopsmicroservices-app --image=$dockerpath
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward pod/devopsmicroservices-app-6994b64c66-g2szc --address 0.0.0.0 8080:80
+
+# Logs
+kubectl logs devopsmicroservices-app-6994b64c66-g2szc
+
